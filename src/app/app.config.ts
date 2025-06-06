3b1f0f9255c5d4e8b438d5d53de82044458ed92d
Kaif <129430112+itskaif07@users.noreply.github.com>
2025-06-07 01:26:33 +0530

    firebase auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,23 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwGLnBqMwvQFJjn9wYZN5n4qUCYZp-_dU",
+  authDomain: "notestalgia-45cd7.firebaseapp.com",
+  projectId: "notestalgia-45cd7",
+  storageBucket: "notestalgia-45cd7.firebasestorage.app",
+  messagingSenderId: "594178840117",
+  appId: "1:594178840117:web:348480457167501a46c37c",
+  measurementId: "G-EWC8LSGX7W"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth())
   ]
 };
