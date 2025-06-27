41c7d79bed4ef3eaba9f21e5b73abfcb03f50621
Kaif <129430112+itskaif07@users.noreply.github.com>
2025-06-27 11:07:24 +0530

    populated data in browse

--- src/app/components/browse/browse.ts
+++ src/app/components/browse/browse.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { NoteCategories } from '../../models/category';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { Notes } from '../../services/notes/notes';
 
 @Component({
   selector: 'app-browse',
@@ -9,28 +10,40 @@ import { CommonModule } from '@angular/common';
   templateUrl: './browse.html',
   styleUrl: './browse.css'
 })
-export class Browse implements OnInit  {
+export class Browse implements OnInit {
 
   categoriesList = NoteCategories
+  notesList: any[] | null = null
 
   imagesList: any[] | null = null
   apiUrl = 'https://picsum.photos/v2/list';
+  noteService = inject(Notes)
 
   http = inject(HttpClient)
 
   ngOnInit(): void {
+    this.getNotes()
     this.getImagesList()
   }
 
-getImagesList() {
- this.http.get(this.apiUrl).subscribe(
-  (response: any) => {
-    this.imagesList = response;
-    console.log(response);
-  },
-  (error) => console.log(error)
-);
+  getImagesList() {
+    this.http.get(this.apiUrl).subscribe(
+      (response: any) => {
+        this.imagesList = response;
+        console.log(response);
+      },
+      (error) => console.log(error)
+    );
+  }
 
-}
+  getNotes() {
+    this.noteService.getAllNotes().subscribe({
+      next: (data) => {
+        this.notesList = data
+        console.log(data)
+      },
+      error: (err) => console.log(err)
+    })
+  }
 
 }
